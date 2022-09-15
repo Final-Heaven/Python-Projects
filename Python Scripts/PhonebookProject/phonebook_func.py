9eb36dea63d4a21647e6568e63cd18ae907a8785
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-09-14 17:46:57 -0700

    Update phonebook_func.py

--- Python Scripts/PhonebookProject/phonebook_func.py
+++ Python Scripts/PhonebookProject/phonebook_func.py
@@ -1,6 +1,7 @@
 import os
 from tkinter import *
 import tkinter as tk
+from tkinter import messagebox
 import sqlite3
 
 # Be sure to import our other modules
@@ -25,3 +26,91 @@ def ask_quit(self):
         # This closes app
         self.master.destroy()
         os._exit(0)
+
+
+#==============================================================================================
+def create_db(self):
+    conn = sqlite3.connect('phonoebook.db')
+    with conn:
+        curr = conn.cursor()
+        cur.execute("CREATE TABLE if not exists tbl_phonebook( \
+            ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+            col_fname TEXT, \
+            col_lname TEXT, \
+            col_fullname TEXT, \
+            col_phone TEXT, \
+            col_email TEXT \
+            );")
+        # You must commit() to save changes and close the database connection
+        conn.commit()
+    conn.close()
+    first_run(self)
+
+
+def first_run(self):
+    conn = sqlite3.connect('phonebook.db')
+    with conn:
+        cur = conn.cursor()
+        cur,count = count_records(cur)
+        if count < 1:
+            cur.execute("""INSERT INTO tbl_phonebook (col_fname, col_lname, col_fullname, col_phone, col_email) VALUES (?, ?, ?, ?, ?)""", ('John', 'Doe', 'John Doe', '111-111-1111', 'jdoe@email.com'))
+            conn.commit()
+    conn.close()
+
+
+def count_records(cur):
+    count = ""
+    cur.execute("""SELECT COUNT(*) FROM tbl_phonebook""")
+    count = cur.fetchone() [0]
+    return cur,count
+
+
+# Select item in ListBox
+def onSelect(self, event):
+    # Calling the event is the self.lstList1 widget
+    varList = event.widget
+    select = varList.curselection() [0]
+    value = varList.get(select)
+    conn = sqlite3.connect('db_phonebook.db')
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute("""SELECT col_fname, col_lname, col_phone, col_email FROM tbl_phonebook WHERE col_fullname = (?)""", [value])
+        varBody = cursor.fetchall()
+        # This returns a tuple and we can slice it into 4 parts using data[] during the iteration
+        for data in varBody:
+            self.txt_fname.delete(0, END)
+            self.txt_fname.insert(0, data[0])
+            self.txt_lname.delete(0, END)
+            self.txt_lname.insert(0, data[1])
+            self.txt_phone.delete(0, END)
+            self.txt_phone.insert(0, data[2])
+            self.txt_email.delete(0, END)
+            self.txt_email.insert(0, data[3])
+
+
+def addToList(self):
+    var_fname = self.txt_fname.get()
+    var_lname = self.txt_lname.get()
+    # Normalize the data to keep it consistent in the database
+    var_fname = var_fname.strip() # This will remove any blank spaces before and after the user's entry
+    var_lname = var_lname.strip()
+    var_fname = var_fname.title() # This will ensure that the first character in each word is capitalized
+    var_lname = var_lname.title()
+    var_fullname = ("{} {}".format(var_fname, var_lname)) # Combine our normalized names into a full name
+    print("var_fullname: {}".format(var_fullname))
+    var_phone = self.txt_phone.get().strip()
+    var_email = self.txt_email.get().strip()
+    if not "@" or not "." in var_email: # Will use this soon
+        print("Incorrect email format!!!")
+    if (len(var_fname) > 0) and (len(var_lname) > 0) and (len(var_phone) > 0) and (len(var_email) > 0): # Enforce the user to provide both names
+        conn = sqlite3.connect('db_phonebook.db')
+        with conn:
+            cursor = conn.cursor()
+
+
+
+
+
+
+
+
