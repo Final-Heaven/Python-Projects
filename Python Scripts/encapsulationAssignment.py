b4ff4eb4aeccb0750a29c1cbb2e7fe1755e08728
Final-Heaven <109561315+Final-Heaven@users.noreply.github.com>
2022-09-16 12:24:02 -0700

    Update encapsulationAssignment.py

--- Python Scripts/encapsulationAssignment.py
+++ Python Scripts/encapsulationAssignment.py
@@ -1,26 +1,29 @@
-
-
+# Creating class
 class ExampleProtect:
     def __init__(self):
-        self._protectedVar = 10
+        self._protectedVar = 10 # Initializing protected variable with value of 10
 
-obj = ExampleProtect()
-obj._protectedVar = 20
+
+obj = ExampleProtect() # Creating object
+obj._protectedVar = 20 # Setting protected variable to new value of 20
 print(obj._protectedVar)
 
+
+# Creating class
 class ExamplePrivate:
     def __init__(self):
-        self.__privateVar = 15
+        self.__privateVar = 15 # Initializing private variable with value of 15
 
-    def get_private(self):
+    def get_private(self): # Function to print the privateVar
         print(self.__privateVar)
 
-    def set_private(self, private):
+    def set_private(self, private): # Function to set value of privateVar
         self.__privateVar = private
 
-obj = ExamplePrivate()
+
+obj = ExamplePrivate() # Creating object
 obj.get_private()
-obj.set_private(30)
+obj.set_private(30) # Setting privateVar to new value and then printing it
 obj.get_private()
 
 
